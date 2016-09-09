d10be3704e4589153754e733876b34b5bf4b6886
tipawan <tipawan.pohk57@sskru.ac.th>
2016-09-09 14:27:54 +0700

    fern

---
+++ opensource/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    class hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Tipawan phokao!5711423147");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
